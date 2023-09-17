e3f559c96ecfaa9044ba3cf125865c105c018265
codekvnnn <kcoronel21@yahoo.com>
2023-09-17 19:46:38 -0400

    chatGPT clone

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <div className="sidebar">
+              <div className="upperSide">
+
+              </div>
+              <div className="lowerSide">
+
+              </div>
+          </div>
+          <div className='main'></div>
+    </div>
+  );
+}
+
+export default App;
